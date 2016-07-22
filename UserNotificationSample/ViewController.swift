5d5ff6ff124e1e99e7c54fdc92c50db1dbb1cd74
Kentaro Matsumae <kentaro.matsumae@dena.com>
2016-07-22 17:29:14 +0900

    Initial Commit

---
+++ UserNotificationSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserNotificationSample
+//
+//  Created by Kentaro Matsumae on 2016/07/22.
+//  Copyright © 2016年 kenmaz.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
